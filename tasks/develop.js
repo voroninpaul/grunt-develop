ee035ee5ab70e3c5c3fc85d66ee05b6ae58124d4
Jean Lauliac <lauliacj@gmail.com>
2013-06-14 13:29:11 -0400

    Add proper handling of non-kill exits.

--- tasks/develop.js
+++ tasks/develop.js
@@ -15,11 +15,14 @@ module.exports = function(grunt) {
 
   var child
     , fs = require('fs')
-    , util = require('util');
+    , util = require('util')
+    , child_process = require('child_process');
 
-  // kills child process (server)
-  grunt.event.on('develop.kill', function() {
-    grunt.log.warn('\nrestarting server');
+  var exited = true;
+
+  // kill child process (server)
+  grunt.event.on('develop.kill', function(filename) {
+    grunt.log.warn('\nkill process');
     child.kill('SIGHUP');
   });
 
@@ -33,26 +36,24 @@ module.exports = function(grunt) {
 
   // starts server
   grunt.event.on('develop.start', function(filename) {
-    if (child && !child.killed) {
-      return grunt.event.emit('develop.kill');
+    if (!exited) {
+      return grunt.event.emit('develop.kill', filename);
     }
-    child = grunt.util.spawn({
-      cmd: process.argv[0],
-      args: [filename]
-    }, function (error, result, code) {
-      grunt.event.emit('develop.start', filename);
+    child = child_process.spawn(process.argv[0], [filename], {
+      stdio: 'inherit'
     });
-    grunt.log.ok(util.format('succesfully started server "%s".', filename));
-    child.stdout.setEncoding('utf8');
-    child.stderr.setEncoding('utf8');
-    child.stdout.on('data', function(data) {
-      grunt.log.write(data);
-    });
-    child.stderr.on('data', function(data) {
-      grunt.log.write(data);
-    });
+    exited = false
+    grunt.log.ok(util.format('started application "%s".', filename));
     child.on('exit', function(code, signal) {
-      grunt.log.warn(util.format('server exited with: "%s"', signal));
+      exited = true;
+      if (signal !== null) {
+        grunt.log.warn(util.format('application exited with signal %s',
+          signal));
+        if (signal === 'SIGHUP')
+          grunt.event.emit('develop.start', filename);
+        return;
+      }
+      grunt.log.warn(util.format('application exited with code %s', code));
     });
     grunt.event.emit('develop.watch', filename);
   });
@@ -61,7 +62,7 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('develop', 'init', function() {
     var done, filename = this.data.file;
     if (!grunt.file.exists(filename)) {
-      grunt.fail.warn(util.format('server file "%s" not found!', filename));
+      grunt.fail.warn(util.format('application file "%s" not found!', filename));
       return false;
     }
     done = this.async();
